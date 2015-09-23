a865e5a6556e3055973f6deb344b67f86c226775
George Kraft <cynerva@gmail.com>
2015-09-23 17:23:36 -0500

    Allow reads to span multiple lines
    What a terrible way to implement this! but it works

--- src/repl.js
+++ src/repl.js
@@ -1,25 +1,28 @@
 var readline = require("readline");
-var read = require("./read");
+
+var Reader = require("./reader");
 var eval = require("./eval");
 
 var prefix = "=> ";
 
 function repl() {
+  var reader = new Reader();
+
   var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: true
   });
 
-  rl.setPrompt(prefix);
-
-  rl.on("line", function(line) {
-    var tree = read(line);
-    var result = eval(tree);
+  reader.onExpression(function(expr) {
+    var result = eval(expr);
     console.log(result);
     rl.prompt();
   });
 
+  rl.on("line", reader.read.bind(reader));
+
+  rl.setPrompt(prefix);
   console.log("foxyjs");
   rl.prompt();
 }
