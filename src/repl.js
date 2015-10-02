31913af5e1b2a815a34c88a663e925318723741c
George Kraft <cynerva@gmail.com>
2015-10-02 17:26:40 -0500

    Add repl

---
+++ src/repl.js
@@ -0,0 +1,40 @@
+var co = require("co");
+var readline = require("readline");
+var makeChannel = require("./channel");
+var makeReadChannel = require("./read").readChannel;
+
+function makeInterface() {
+  return readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+}
+
+function makeLineChannelFromInterface(interface) {
+  var ch = makeChannel();
+
+  interface.on("line", function(line) {
+    ch.put(line);
+  });
+
+  return ch;
+}
+
+function startRepl() {
+  var interface = makeInterface();
+  var lineChannel = makeLineChannelFromInterface(interface);
+  var readChannel = makeReadChannel(lineChannel);
+  console.log("foxyjs");
+  interface.setPrompt("=> ");
+  interface.prompt();
+
+  co(function*() {
+    while (true) {
+      var expr = yield readChannel.take();
+      console.log(expr);
+      interface.prompt();
+    }
+  });
+}
+
+module.exports = startRepl;
