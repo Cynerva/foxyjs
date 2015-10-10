b03e2e463cf475f2fcd3fd276db1e96b9f093cb7
George Kraft <cynerva@gmail.com>
2015-10-10 13:12:19 -0500

    refactor eval tests to use strings

--- test/eval.js
+++ test/eval.js
@@ -1,53 +1,63 @@
 var assert = require("assert");
+var co = require("co");
+var mocha = require("mocha");
 var mori = require("mori");
+var helper = require("./helper");
+var readString = require("../src/read").readString;
 
+var describe = mocha.describe;
+var equals = mori.equals;
+var it = helper.it;
 var list = mori.list;
 
 var eval = require("../src/eval");
 
 describe("eval", function() {
-  it("evaluates arithmetic", function() {
-    var ast = list("+", 1, list("*", 2, 3));
-    var expected = 7;
-    assert(eval(ast) === expected);
+  function evalStr(str) {
+    return co(function*() {
+      var ast = yield readString(str);
+      return eval(ast);
+    });
+  }
+
+  it("evaluates arithmetic", function*() {
+    var result = yield evalStr("(+ 1 (* 2 3))");
+    assert(result === 7);
   });
 
-  it("evaluates code that returns a list", function() {
-    var ast = list("quote", list());
-    var result = eval(ast);
-    assert(mori.equals(result, list()));
+  it("evaluates code that returns a list", function*() {
+    var result = yield evalStr("'()");
+    assert(equals(result, list()));
   });
 
-  it("can def then dereference a symbol", function() {
-    eval(list("def", "a", 1));
-    var result = eval("a");
+  it("can def then dereference a symbol", function*() {
+    yield evalStr("(def a 1)");
+    var result = yield evalStr("a");
     assert(result === 1);
   });
 
-  it("can defmacro then use that macro", function() {
-    eval(list("defmacro", "foo", list(), 1));
-    var result = eval(list("foo"));
+  it("can defmacro then use that macro", function*() {
+    yield evalStr("(defmacro foo () 1)");
+    var result = yield evalStr("(foo)");
     assert(result === 1);
   });
 
-  it("can eval a macro with two args", function() {
-    var args = list("a", "b");
-    var body = list("+", "a", "b");
-    eval(list("defmacro", "foo", args, body));
-    var result = eval(list("foo", 1, 2));
+  it("can eval a macro with two args", function*() {
+    yield evalStr("(defmacro foo (a b) (+ a b))");
+    var result = yield evalStr("(foo 1 2)");
     assert(result === 3);
   });
 
-  it("can eval a macro with backquote", function() {
-    eval(list("defmacro", "foo", list(), list("backquote", list("+", 1, 2))));
-    var result = eval(list("foo"));
+  it("can eval a macro with backquote", function*() {
+    yield evalStr("(defmacro foo () `(+ 1 2))");
+    var result = yield evalStr("(foo)");
     assert(result === 3);
   });
 
-  it("can eval a macro that refers to another symbol", function() {
-    eval(list("def", "a", 5));
-    eval(list("defmacro", "foo", list(), "a"));
-    var result = eval(list("foo"));
+  it("can eval a macro that refers to another symbol", function*() {
+    yield evalStr("(def a 5)");
+    yield evalStr("(defmacro foo () a)");
+    var result = yield evalStr("(foo)");
     assert(result === 5);
   });
 });
